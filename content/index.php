bf1f31276215d1b4b229c4d2427579cf01d805f9
trinhthianh1273 <anh.tt.2035@aptechlearning.edu.vn>
2022-08-02 06:41:07 +0700

    tranh:test3

--- content/index.php
+++ content/index.php
@@ -15,18 +15,20 @@
 	<script src="assets/js/angular.min.js"></script>
 	<script src="assets/js/angular-route.min.js"></script>
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
 	<!-- My App -->
 	<script src="app/routes.js"></script>
 	<script src="app/helper/myHelper.js"></script>
 </head>
 <body ng-app="my-App">
-    <?php require_once $_SERVER['DOCUMENT_ROOT']."/BidSpirit/content/views/header.html"; ?>
+    <?php require_once $_SERVER['DOCUMENT_ROOT']."/group1/content/views/header.html"; ?>
     <div class="container">
         <ng-view>
             
         </ng-view>
     </div>
 
-    <?php require_once $_SERVER['DOCUMENT_ROOT']."/BidSpirit/content/views/footer.html"; ?>
+    <?php require_once $_SERVER['DOCUMENT_ROOT']."/group1/content/views/footer.html"; ?>
 </body>
 </html>
