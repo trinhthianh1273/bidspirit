8050c43e91fe0ca5660dc3d59aed96cb49d6ddd6
trinhthianh1273 <anh.tt.2035@aptechlearning.edu.vn>
2022-08-04 21:06:33 +0700

    tranh:sql full

--- content/DB_config.php
+++ content/DB_config.php
@@ -4,5 +4,6 @@ define ('DB_PASSWORD', "");
 define ('DB_DATABASE', "BidSpirit");
 define ('DB_HOST', "localhost");
 $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
-
+$sql = "drop database BidSpirit";
+$mysqli->query($sql);
 ?>
