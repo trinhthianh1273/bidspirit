2873a610c89a8e4eb469f3642f052e3dd3665206
Bao Anh <anh.lb.2041@aptechlearning.edu.vn>
2022-08-19 08:33:54 +0700

    Bao Anh update file closedProduct.js + php

---
+++ content/app/controllers/closedProduct.js
@@ -0,0 +1,13 @@
+app.controller('closedProduct', function($scope,$http){
+    $scope.closedauction = [];
+    viewProduct();
+    function viewProduct() {
+        $http({
+            url: URL + "api/interview/closedProduct.php",
+            method: 'GET'
+          }).then(function(res){
+            $scope.viewProduct = res.data.viewProduct;
+          });
+    }
+})
+// View mới thêm từ closedaution
